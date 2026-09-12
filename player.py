366d66820683eb2875bf12cca273d686bfdb590a
Adam Thelin <adam.timra@hotmail.com>
2026-09-12 12:17:48 +0200

    add shot with rotation

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from shot import Shot
 import pygame
 
 class Player(CircleShape):
@@ -32,9 +33,18 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt: float):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+
+    def shoot(self) -> None:
+        current_shot = Shot(self.position.x, self.position.y)
+
+        current_shot.velocity = pygame.Vector2(0, 1)
+        current_shot.velocity = current_shot.velocity.rotate(self.rotation)
+        current_shot.velocity *= PLAYER_SHOOT_SPEED
